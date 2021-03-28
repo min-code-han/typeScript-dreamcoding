a308ed5bf289eb1866c62872a1dfcaea3c715ccd
mina <yegrinaaa@gmail.com>
2021-03-29 00:21:41 +0900

    0329 | function type

---
+++ 1-types/1-2-function.ts
@@ -0,0 +1,31 @@
+{
+  // JavaScript
+  function jsAdd(num1, num2) {
+    return num1 + num2;
+  }
+  // TypeScript
+  function add(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+
+  //JavaScript
+  function jsFetchNum(id) {
+    // code...
+    // code...
+    // code...
+    // code...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+    //TypeScript
+    function FetchNum(id: string): Promise<number> {
+      // code...
+      // code...
+      // code...
+      // code...
+      return new Promise((resolve, reject) => {
+        resolve(100);
+      });
+    }
+  }
+}
