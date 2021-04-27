45c6d6d6c5ae8e8e677f58c58ddec155cee7fa34
mina <yegrinaaa@gmail.com>
2021-04-27 21:22:06 +0900

    branch 정리

--- 1-types/1-2-function.ts
+++ 1-types/1-2-function.ts
@@ -1,33 +1,33 @@
 {
   // // JavaScript
-  // function jsAdd(num1, num2) {
-  //   return num1 + num2;
-  // }
+  function jsAdd(num1, num2) {
+    return num1 + num2;
+  }
   // // TypeScript
-  // function add(num1: number, num2: number): number {
-  //   return num1 + num2;
-  // }
+  function add(num1: number, num2: number): number {
+    return num1 + num2;
+  }
 
   // //JavaScript
-  // function jsFetchNum(id) {
-  //   // code...
-  //   // code...
-  //   // code...
-  //   // code...
-  //   return new Promise((resolve, reject) => {
-  //     resolve(100);
-  //   });
-  // }
+  function jsFetchNum(id) {
+    // code...
+    // code...
+    // code...
+    // code...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
   // //TypeScript
-  // function FetchNum(id: string): Promise<number> {
-  //   // code...
-  //   // code...
-  //   // code...
-  //   // code...
-  //   return new Promise((resolve, reject) => {
-  //     resolve(100);
-  //   });
-  // }
+  function FetchNum(id: string): Promise<number> {
+    // code...
+    // code...
+    // code...
+    // code...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
 
   // JavaScript + TypeScript
 
